0d8c8dd0bfb16b7184fdbde15cf1368d28853e36
munadifathullah123 <munadifathullah123@gmail.com>
2024-05-19 01:15:03 +0000

    feat: add server adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
-    applyBaseStyles: false,
+    applyBaseStyles: false
   }),, react()],
-  output: "server"
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
