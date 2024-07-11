0f7fc68da293aad6a78ae10bc90ba4635cef5b27
munadifathullah123 <munadifathullah123@gmail.com>
2024-07-11 11:04:23 +0000

    test adpater vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import node from "@astrojs/node";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,7 +11,5 @@ export default defineConfig({
     applyBaseStyles: false
   }),, react()],
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: vercel()
 });
